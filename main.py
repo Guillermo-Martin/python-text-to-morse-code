b351ceac4445fcc232b1bb681e2acee9e25fb2d7
Guillermo Martin <gscalica@gmail.com>
2026-09-23 13:50:23 -0700

    Add comments and remove the last space from the Morse Code string

--- main.py
+++ main.py
@@ -39,6 +39,19 @@ MORSE_CODE = {
   " ": "/"
 }
 
-# ask user for a word to convert
-word_to_convert = input("Enter the word to convert: ").lower()
-print(word_to_convert)
+# variable to hold the morse code
+converted_text = ""
+
+# ask user for a text to convert
+text_to_convert = input("Enter text to convert: ").lower()
+
+# loop through each character in the text
+for char in text_to_convert:
+  converted_text += MORSE_CODE[char]
+
+  # add space between letters so it's more readable
+  converted_text += " "
+
+# show final message
+# "[:-1]" removes the last space from the string
+print(f"Here's the Morse Code for your text: {converted_text[:-1]}")
